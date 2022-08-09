d48456008720beb723ab776416471682fdab5ec5
Haleemah Korodo <haleemahkorodo@gmail.com>
2022-08-09 21:49:46 +0100

    commit responsive

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ for   ( i=0; i < acc.length; i++ ){
         }
     })
 }
+
+
+
+var object = document.getElementsByClassName("object");
+var button = document.getElementsByClassName("popup")
+
+var i;
+
+for   ( i=0; i < acc.length; i++ ){
+    button[i].addEventListener("click", function() {
+         
+        // toggle between adding and removing the active class
+        this.classList.toggle("active");
+
+        //toggle between hiding and showing the answer
+        var object = this.nextElementSibling;
+        console.log(object)
+        if (object.style.display === "block") {
+            object.style.display = "none";
+        } else {
+            object.style.display = "block";
+        }
+    })
+}
